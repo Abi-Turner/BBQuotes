a53bc2e5c470b3f4f6f24a57de1543ff8af66c16
Abi Turner <turner.abi@gmail.com>
2024-10-16 13:31:24 +0100

    Initial Commit

---
+++ BBQuotes/BBQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBQuotesApp.swift
+//  BBQuotes
+//
+//  Created by Abigail Turner on 16/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct BBQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
